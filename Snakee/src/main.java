4cdb6ce8b02227fa37ec3ee96e784fd708893b41
Dayvedde <dayvedde@hotmail.com>
2014-12-05 22:24:01 -0500

    Testing some stuff

--- Snakee/src/main.java
+++ Snakee/src/main.java
@@ -7,6 +7,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello Hello Hello Mello Yello Pello Chello Fello Peep");
+		TextDisplay td = new TextDisplay ();
+		td.printDisplay();
 	}
 
 }
